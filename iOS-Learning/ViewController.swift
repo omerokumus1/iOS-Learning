c518823674f4e690abe0aeba31a0956f51b221d1
omerokumus <omerokumus3@gmail.com>
2023-04-11 23:57:16 +0300

    NoRecordView is added to ViewController programatically

--- iOS-Learning/ViewController.swift
+++ iOS-Learning/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let noRecordView = NoRecordView(frame: CGRect(x: 20.0, y: 100.0, width: 300.0, height: 300.0))
+        self.view.addSubview(noRecordView)
     }
 
 
