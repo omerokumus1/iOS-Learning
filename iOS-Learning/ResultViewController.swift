93c2d8754cb5a825d55e722f76d8447410cb10a7
Ömer Okumuş <omerokumus3@gmail.com>
2023-04-12 18:39:56 +0300

    ResultViewController is created

---
+++ iOS-Learning/ResultViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ResultViewController.swift
+//  iOS-Learning
+//
+//  Created by Ömer Faruk Okumuş on 12.04.2023.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    @IBOutlet weak var tlLabel: UILabel!
+    @IBOutlet weak var dollarLabel: UILabel!
+    @IBOutlet weak var euroLabel: UILabel!
+    
+    var tl: String?, dollar: String?, euro: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tlLabel.text = tl
+        dollarLabel.text = dollar
+        euroLabel.text = euro
+    }
+    
+}
