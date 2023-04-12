bfc96297cc94160f0777d9517e62bbea83e083d8
Ömer Okumuş <omerokumus3@gmail.com>
2023-04-12 18:39:26 +0300

    Router is created

---
+++ iOS-Learning/Router.swift
@@ -0,0 +1,27 @@
+//
+//  Router.swift
+//  iOS-Learning
+//
+//  Created by Ömer Faruk Okumuş on 12.04.2023.
+//
+
+import UIKit
+
+class Router: PresenterToRouterProtocol {
+        
+    func showResultScreen(payload: Array<String>) {
+        let  mainStoryboard =
+        (UIApplication.shared.windows.filter {$0.isKeyWindow}.first?.rootViewController?.storyboard)!
+        let resultVC = mainStoryboard.instantiateViewController(
+            withIdentifier: "ResultViewController"
+        ) as! ResultViewController
+        resultVC.tl = payload[0]
+        resultVC.dollar = payload[1]
+        resultVC.euro = payload[2]
+        
+        let currentVC = UIApplication.shared.delegate?.window??.rootViewController
+        currentVC?.present(resultVC, animated: true)
+    }
+    
+    
+}
