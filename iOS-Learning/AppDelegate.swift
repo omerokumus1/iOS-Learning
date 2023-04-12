5dd7d8a42042e695f548d9d7d7f6f1bda881ae77
Ömer Okumuş <omerokumus3@gmail.com>
2023-04-12 18:40:15 +0300

    AppDelegate window operations

--- iOS-Learning/AppDelegate.swift
+++ iOS-Learning/AppDelegate.swift
@@ -10,10 +10,15 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = UIStoryboard(name:"Main",bundle: Bundle.main)
+            .instantiateViewController(withIdentifier: "ViewController")
+        window?.makeKeyAndVisible()
+        
         return true
     }
 
