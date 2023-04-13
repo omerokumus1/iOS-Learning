4402b0426c41a9cf0a52d8ebdff691bbeb3dafba
Ömer Okumuş <omerokumus3@gmail.com>
2023-04-13 22:48:47 +0300

    NoRecorView is fixed

--- iOS-Learning/ViewController.swift
+++ iOS-Learning/ViewController.swift
@@ -14,11 +14,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-//        let noRecordView = NoRecordView(
-//            frame: CGRect(x: 20.0, y: 100.0, width: 300.0, height: 300.0)
-//        )
-        
-        //self.view.addSubview(noRecordView)
+        let noRecordView = NoRecordView(
+            frame: CGRect(x: 20.0, y: 100.0, width: 300.0, height: 300.0)
+        )
+        noRecordView.lblNoRecord.text = "Not Found"
+        self.view.addSubview(noRecordView)
         
         let checkbox = Checkbox(frame: CGRect(x: 50, y: 50, width: 200, height: 200))
         view.addSubview(checkbox)
