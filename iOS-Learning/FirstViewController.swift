fc845f9b86050c52b6f3cf650efd77141b134359
Ömer Okumuş <omerokumus3@gmail.com>
2023-04-17 23:07:52 +0300

    Second view controller is created with storyboard?.instantiateViewController and started

---
+++ iOS-Learning/FirstViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  iOS-Learning
+//
+//  Created by Ömer Faruk Okumuş on 11.04.2023.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    private let router = Router()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    
+    @IBAction func nextClicked(_ sender: UIButton) {
+//        let secondVC = UIStoryboard.instantiateViewController(
+//            storyboard!
+//        )(withIdentifier: "SecondViewController") as! SecondViewController
+//
+        let secondVC = storyboard?.instantiateViewController(
+            withIdentifier: "SecondViewController"
+        ) as! SecondViewController
+        self.present(secondVC, animated: true)
+
+    }
+    
+    
+    
+}
+
