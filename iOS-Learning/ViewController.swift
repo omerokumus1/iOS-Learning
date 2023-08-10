242d2f5e627c35332f8117baf52f47d13a51f339
Ömer Okumuş <omerokumus3@gmail.com>
2023-08-10 08:08:02 +0300

    textview

--- iOS-Learning/ViewController.swift
+++ iOS-Learning/ViewController.swift
@@ -9,11 +9,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tf: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tf.delegate = self
+        view.addSubview(tf)
+        
     }
 
 
 }
 
+extension ViewController: UITextFieldDelegate {
+    // Girilen harf eklensin mi kontrolü için
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        // get the current text, or use an empty string if that failed
+        let currentText = textField.text ?? ""
+        
+        // attempt to read the range they are trying to change, or exit if we can't
+        guard let stringRange = Range(range, in: currentText) else { return false }
+        
+        // add their new text to the existing text
+        let updatedText = currentText.replacingCharacters(in: stringRange, with: string.uppercased())
+        print("updatedText: \(updatedText)")
+        print("textField.text: \(textField.text)")
+        print()
+        // make sure the result is under 16 characters
+        return true
+    }
+    
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        return false
+    }
+    
+    func textFieldDidChangeSelection(_ textField: UITextField) {
+        print("changed")
+    }
+    
+    
+}
