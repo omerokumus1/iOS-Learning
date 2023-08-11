b14c83e7e8dfe3da9419bb0d28ee7b1a2fad2c2b
Ömer Okumuş <omerokumus3@gmail.com>
2023-08-11 08:22:28 +0300

    slider

--- iOS-Learning/ViewController.swift
+++ iOS-Learning/ViewController.swift
@@ -12,6 +12,32 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let slider = UISlider(frame: CGRect(x: 100, y: 100, width: 200, height: 5))
+        
+        //slider.backgroundColor = .red
+        slider.thumbTintColor = .blue
+        slider.trackRect(forBounds: slider.frame)
+        slider.minimumValue = 0
+        slider.maximumValue = 100
+        slider.isContinuous = true
+        
+        slider.addTarget(self, action: #selector(sliderFunc), for: .valueChanged)
+        
+        
+        view.addSubview(slider)
+    }
+    
+    @objc func sliderFunc() {
+        print("sliderFunc")
+    }
+    
+    @objc func sliderFunc2(sender: UISlider!) {
+        print("sliderFunc2")
+    }
+    
+    @objc func sliderFunc3(sender: UISlider, event: UIEvent) {
+        print("sliderFunc3")
     }
 
 
