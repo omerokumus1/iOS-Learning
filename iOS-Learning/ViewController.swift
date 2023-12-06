23a69ba53375c9afdf37cb8dde3dbb3a71922051
Ömer Okumuş <omerokumus3@gmail.com>
2023-12-06 08:28:56 +0300

    UICalendarView

--- iOS-Learning/ViewController.swift
+++ iOS-Learning/ViewController.swift
@@ -12,6 +12,33 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let calendarView = UICalendarView(frame: 
+                                            CGRect(x: 50, y: 50,
+                                                   width: 300, height: 300))
+        
+        
+        let fromDateComponents = DateComponents(calendar: 
+                                                    Calendar(identifier: .gregorian),
+                                                year: 2022, month: 1, day: 1)
+        let toDateComponents = DateComponents(calendar: 
+                                                Calendar(identifier: .gregorian),
+                                              year: 2022, month: 12, day: 31)
+        
+        
+        guard let fromDate = fromDateComponents.date, 
+                let toDate = toDateComponents.date else {
+            return
+        }
+        
+        
+        let calendarViewDateRange = DateInterval(start: fromDate, end: toDate)
+        calendarView.availableDateRange = calendarViewDateRange
+        
+        
+        self.view.addSubview(calendarView)
+        
+        
     }
 
 
