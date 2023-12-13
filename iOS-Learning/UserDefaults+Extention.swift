176b89373f3942789fc8004bf7de672dfe5c1214
Ömer Okumuş <omerokumus3@gmail.com>
2023-12-13 07:18:41 +0300

    UserDefaults

--- iOS-Learning/UserDefaults+Extention.swift
+++ iOS-Learning/UserDefaults+Extention.swift
@@ -6,3 +6,40 @@
 //
 
 import Foundation
+
+extension UserDefaults {
+    private enum Keys: String {
+        case switchIsOn
+        case signedInUser
+    }
+    
+    var switchIsOn: Bool {
+        get {
+            self.bool(forKey: Keys.switchIsOn.rawValue)
+        }
+        set {
+            self.setValue(newValue, forKey: Keys.switchIsOn.rawValue)
+        }
+    }
+    
+    
+    var signedInUser: User? {
+        get {
+            if let data = self.object(forKey: Keys.signedInUser.rawValue) as? Data {
+                let user = try? JSONDecoder().decode(User.self, from: data)
+                return user
+            }
+            return nil
+        }
+        
+        set {
+            if newValue == nil {
+                self.removeObject(forKey: Keys.signedInUser.rawValue)
+            }
+            
+            let data = try? JSONEncoder().encode(newValue)
+            self.setValue(data, forKey: Keys.signedInUser.rawValue)
+            
+        }
+    }
+}
