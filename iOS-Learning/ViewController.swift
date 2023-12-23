dc201413abaa126b61587774e4dbe8e2d1ee8529
Ömer Okumuş <omerokumus3@gmail.com>
2023-12-23 10:19:38 +0300

    get, post, put, patch, delete, sending parameters, converting from/to Swift object using alamofire

--- iOS-Learning/ViewController.swift
+++ iOS-Learning/ViewController.swift
@@ -7,13 +7,64 @@
 
 import UIKit
 
+import Alamofire
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        AF.request("https://jsonplaceholder.typicode.com/posts/1")
+            .response { response in
+                debugPrint(response)
+            }
+        
+        AF.request("https://jsonplaceholder.typicode.com/posts/1")
+            .responseDecodable(of: Post.self) { response in
+                // post is Post object
+                guard let post = response.value else {return}
+                print(post)
+            }
+        
+        let parameters = ["category": "Movies", "genre": "Action"]
+        AF.request("https://jsonplaceholder.typicode.com/posts/1",
+                   parameters: parameters)
+        
+        // POST
+        AF.request("https://httpbin.org/post", method: .post, parameters: parameters)
+            .response { response in
+                debugPrint(response)
+            }
+        // PUT
+        AF.request("https://httpbin.org/put", method: .put, parameters: parameters)
+            .response { response in
+                debugPrint(response)
+            }
+        // DELETE
+        AF.request("https://httpbin.org/delete", method: .delete).response { response in
+            debugPrint(response)
+        }
+        
+        let post = Post(userId: 1, id: 1, 
+                        title: "title", body: "body")
+        AF.request("https://httpbin.org/post",
+                   parameters: post)
+        
+        AF.request("https://httpbin.org/post",
+                   method: .post,
+                   parameters: post)
+        
     }
 
 
 }
 
+struct Post: Codable {
+    let userId, id: Int
+    let title, body: String
+}
+
+struct Posts: Codable {
+    let posts: [Post]
+}
