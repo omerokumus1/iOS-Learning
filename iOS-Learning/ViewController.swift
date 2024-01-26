fa8c38f1d73182d3005dd48da602d617a2ecb3c3
Ömer Okumuş <omerokumus3@gmail.com>
2024-01-26 19:01:53 +0300

    statusbar

--- iOS-Learning/ViewController.swift
+++ iOS-Learning/ViewController.swift
@@ -8,10 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
 
