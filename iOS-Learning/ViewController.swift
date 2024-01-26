176c385f7fdb2a50fd9c39364b98a265c23cfd53
Ömer Okumuş <omerokumus3@gmail.com>
2024-01-26 11:59:15 +0300

    toast

--- iOS-Learning/ViewController.swift
+++ iOS-Learning/ViewController.swift
@@ -6,14 +6,46 @@
 //
 
 import UIKit
+import Toast
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var button: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    @IBAction func onClickButton(_ sender: UIButton) {
+        //view.makeToast("Toast Message")
+        //view.makeToast("This is a piece of toast", duration: 3.0, position: .top)
+        
+        // toast presented with multiple options and with a completion closure
+//        view.makeToast("This is a piece of toast", 
+//                       duration: 2.0,
+//                       point: CGPoint(x: 150.0, y: 110.0),
+//                       title: "Toast Title",
+//                       image: UIImage(named: "avatar-male.png"))
+//        { didTap in
+//            if didTap {
+//                print("completion from tap")
+//            } else {
+//                print("completion without tap")
+//            }
+//        }
+        
+        // display toast with an activity spinner
+        self.view.makeToastActivity(.center)
+        Timer.scheduledTimer(withTimeInterval: 3.0, repeats: false) { timer in
+            self.view.hideToastActivity()
+        }
+        
+        // display any view as toast
+//        self.view.showToast(myView)
+        
+        
+    }
+    
 }
 
