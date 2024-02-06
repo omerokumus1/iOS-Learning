0a6e443d50362c54c5674b41e3c53fba9e5c4aef
Ömer Okumuş <omerokumus3@gmail.com>
2024-02-06 16:14:13 +0300

    gradient text color

--- iOS-Learning/ViewController.swift
+++ iOS-Learning/ViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let gradient = getGradientLayer(bounds: label.bounds)
+        label.textColor = gradientColor(bounds: label.bounds, gradientLayer: gradient)
     }
-
-
+    
+    func getGradientLayer(bounds : CGRect) -> CAGradientLayer{
+        let gradient = CAGradientLayer()
+        gradient.frame = bounds
+        //order of gradient colors
+        gradient.colors = [UIColor.red.cgColor, UIColor.blue.cgColor, UIColor.green.cgColor]
+        // start and end points
+        gradient.startPoint = CGPoint(x: 0.0, y: 0.5)
+        gradient.endPoint = CGPoint(x: 1.0, y: 0.5)
+        return gradient
+    }
+    
+    func gradientColor(bounds: CGRect, gradientLayer :CAGradientLayer) -> UIColor? {
+        UIGraphicsBeginImageContext(gradientLayer.bounds.size)
+        //create UIImage by rendering gradient layer.
+        gradientLayer.render(in: UIGraphicsGetCurrentContext()!)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        //get gradient UIcolor from gradient UIImage
+        return UIColor(patternImage: image!)
+    }
+    
+    
 }
 
