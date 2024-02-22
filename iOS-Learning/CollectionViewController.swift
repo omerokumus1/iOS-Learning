4394ab937b46f29ce28d6d0464c4c0d5f9220b31
Ömer Okumuş <omerokumus3@gmail.com>
2024-02-22 21:13:16 +0300

    broken version

--- iOS-Learning/CollectionViewController.swift
+++ iOS-Learning/CollectionViewController.swift
@@ -7,13 +7,158 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    private var indexOfCellBeforeDragging = 0
+    
+    let colors = [UIColor.black, UIColor.blue, UIColor.brown,
+                  UIColor.cyan, UIColor.darkGray, UIColor.gray,
+                  UIColor.green, UIColor.magenta, UIColor.orange,
+                  UIColor.purple, UIColor.red, UIColor.white, UIColor.yellow, UIColor.blue, UIColor.brown,
+                  UIColor.cyan, UIColor.darkGray, UIColor.gray,
+                  UIColor.green, UIColor.magenta, UIColor.orange,
+                  UIColor.purple, UIColor.red, UIColor.white, UIColor.yellow, UIColor.blue, UIColor.brown,]
 
+    var collectionViewLayout = UICollectionViewFlowLayout()
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    @IBOutlet weak var pageControl: UIPageControl!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        collectionViewLayout.minimumLineSpacing = 0
+        collectionViewLayout.scrollDirection = .horizontal
+        collectionView.collectionViewLayout = collectionViewLayout
+        collectionView.dataSource = self
+        collectionView.delegate = self
     }
-
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        configureCollectionViewLayoutItemSize()
+    }
+    
+    private func calculateSectionInset() -> CGFloat {
+        let deviceIsIpad = UIDevice.current.userInterfaceIdiom == .pad
+        let deviceOrientationIsLandscape = UIDevice.current.orientation.isLandscape
+        let cellBodyViewIsExpended = deviceIsIpad || deviceOrientationIsLandscape
+        let cellBodyWidth: CGFloat = 236 + (cellBodyViewIsExpended ? 174 : 0)
+        
+        let buttonWidth: CGFloat = 50
+        
+        let inset = (collectionViewLayout.collectionView!.frame.width - cellBodyWidth + buttonWidth) / 4
+        return inset
+    }
+    
+    private func configureCollectionViewLayoutItemSize() {
+        let inset: CGFloat = calculateSectionInset() // This inset calculation is some magic so the next and the previous cells will peek from the sides. Don't worry about it
+        collectionViewLayout.sectionInset = UIEdgeInsets(top: 0, left: inset, bottom: 0, right: inset)
+        
+        collectionViewLayout.itemSize = CGSize(width: collectionViewLayout.collectionView!.frame.size.width - inset * 2, height: collectionViewLayout.collectionView!.frame.size.height)
+    }
+    
+    private func indexOfMajorCell() -> Int {
+        let itemWidth = collectionViewLayout.itemSize.width
+        let proportionalOffset = collectionViewLayout.collectionView!.contentOffset.x / itemWidth
+        let index = Int(round(proportionalOffset))
+        let safeIndex = max(0, min(colors.count - 1, index))
+        return safeIndex
+    }
+    
+    // ===================================
+    // MARK: - UICollectionViewDataSource:
+    // ===================================
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return colors.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! MyCollectionCell
+        
+        cell.view.backgroundColor = colors[indexPath.row]
+        
+        
+        
+        // You can color the cells so you could see how they behave:
+        //        let isEvenCell = CGFloat(indexPath.row).truncatingRemainder(dividingBy: 2) == 0
+        //        cell.backgroundColor = isEvenCell ? UIColor(white: 0.9, alpha: 1) : .white
+        
+        return cell
+    }
+    
+    // =================================
+    // MARK: - UICollectionViewDelegate:
+    // =================================
+    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+        indexOfCellBeforeDragging = indexOfMajorCell()
+        print("indexOfCellBeforeDragging: \(indexOfCellBeforeDragging)")
+    }
+    
+    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        // Stop scrollView sliding:
+        targetContentOffset.pointee = scrollView.contentOffset
+        
+        // calculate where scrollView should snap to:
+        let indexOfMajorCell = self.indexOfMajorCell()
+        print("indexOfMajorCell: \(indexOfMajorCell)")
+        
+        // calculate conditions:
+        let swipeVelocityThreshold: CGFloat = 0.5 // after some trail and error
+        let hasEnoughVelocityToSlideToTheNextCell = indexOfCellBeforeDragging + 1 < colors.count && velocity.x > swipeVelocityThreshold
+        let hasEnoughVelocityToSlideToThePreviousCell = indexOfCellBeforeDragging - 1 >= 0 && velocity.x < -swipeVelocityThreshold
+        let majorCellIsTheCellBeforeDragging = indexOfMajorCell == indexOfCellBeforeDragging
+        let didUseSwipeToSkipCell = // majorCellIsTheCellBeforeDragging &&
+        true && (hasEnoughVelocityToSlideToTheNextCell || hasEnoughVelocityToSlideToThePreviousCell || true)
+        
+        if didUseSwipeToSkipCell {
+            
+            let snapToIndex = indexOfCellBeforeDragging + (hasEnoughVelocityToSlideToTheNextCell ? 1 : -1)
+            let toValue = collectionViewLayout.itemSize.width * CGFloat(snapToIndex)
+            
+            // Damping equal 1 => no oscillations => decay animation:
+            UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: velocity.x, options: .allowUserInteraction, animations: {
+                scrollView.contentOffset = CGPoint(x: toValue, y: 0)
+                scrollView.layoutIfNeeded()
+            }, completion: nil)
+            
+        } else {
+            // This is a much better way to scroll to a cell:
+            let indexPath = IndexPath(row: indexOfMajorCell, section: 0)
+            collectionViewLayout.collectionView!.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
+        }
+    }
+    
+    
+ /*   override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        var flowLayout = UICollectionViewFlowLayout()
+        flowLayout.itemSize = CGSize(width: UIScreen.main.bounds.width - 20, height: 190)
+        flowLayout.sectionInset = UIEdgeInsets(top: 0, left: 5, bottom: 0, right: 5)
+        flowLayout.scrollDirection = .horizontal
+        flowLayout.minimumInteritemSpacing = 10.0
+        collectionView.collectionViewLayout = flowLayout
+        
+        pageControl.numberOfPages = colors.count
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return colors.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+//        cell.contentView.subviews.first?.backgroundColor = colors[indexPath.row]
+        cell.backgroundColor = colors[indexPath.row]
+        pageControl.currentPage = indexPath.row
+        return cell
+    }
+*/
 
 }
-
