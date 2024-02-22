02f75f9c1441d92a266c3646852736abf053a391
Ömer Okumuş <omerokumus3@gmail.com>
2024-02-22 21:13:01 +0300

    broken version

---
+++ iOS-Learning/MyCollectionCell.swift
@@ -0,0 +1,12 @@
+//
+//  MyCollectionCellCollectionViewCell.swift
+//  iOS-Learning
+//
+//  Created by Ömer Faruk Okumuş on 22.02.2024.
+//
+
+import UIKit
+
+class MyCollectionCellCollectionViewCell: UICollectionViewCell {
+    
+}
