ed22db022b48f69928f4096a8d6c9a98b2c68704
Ömer Okumuş <omerokumus3@gmail.com>
2024-02-24 16:50:16 +0300

    ui-collection-view

--- iOS-Learning/ViewController.swift
+++ iOS-Learning/ViewController.swift
@@ -9,11 +9,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let colors = [UIColor.red, UIColor.blue, UIColor.green, UIColor.yellow, UIColor.orange, UIColor.purple, UIColor.red, UIColor.blue, UIColor.green, UIColor.yellow, UIColor.orange, UIColor.purple, ]
+    @IBOutlet weak var collectionView: UICollectionView!
+    var layout = UICollectionViewFlowLayout()
+    var currentIndex = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        layout.scrollDirection = .horizontal
+        layout.itemSize = CGSize(width: collectionView.bounds.width-80, height: 128)
+        layout.minimumInteritemSpacing = 0
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 40)
+        
+        collectionView.collectionViewLayout = layout
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.isPagingEnabled = true
+        
         // Do any additional setup after loading the view.
     }
 
 
 }
 
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return colors.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = colors[indexPath.row]
+        return cell
+    }
+}
+
+extension ViewController: UICollectionViewDelegate {
+    
+    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+        let itemWidth = layout.itemSize.width
+        let proportionalOffset = layout.collectionView!.contentOffset.x / itemWidth
+        let index = Int(round(proportionalOffset))
+        let numberOfItems = collectionView.numberOfItems(inSection: 0)
+        let safeIndex = max(0, min(numberOfItems - 1, index))
+        
+    }
+    
+    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        
+    }
+}
