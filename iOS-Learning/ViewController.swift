47a74e32f9c756cea37d7b3c5068a7a3677265c8
Ömer Okumuş <omerokumus3@gmail.com>
2024-11-09 12:20:03 +0300

    layout guide

--- iOS-Learning/ViewController.swift
+++ iOS-Learning/ViewController.swift
@@ -17,3 +17,27 @@ class ViewController: UIViewController {
 
 }
 
+// Create a protocol to act as an image destination.
+protocol ImageContainer {
+    func configureImage(_ image: UIImage)
+}
+
+// Make both `UIButton` and `UIImageView` conform to the protocol.
+extension UIButton: ImageContainer {
+    func configureImage(_ image: UIImage) {
+        setImage(image, for: .normal)
+    }
+}
+
+extension UIImageView: ImageContainer {
+    func configureImage(_ image: UIImage) {
+        self.image = image
+    }
+}
+
+// Create a new method using the protocol as a destination.
+func configureImage(_ image: UIImage, into destinations: [ImageContainer]) {
+    for destination in destinations {
+        destination.configureImage(image)
+    }
+}
