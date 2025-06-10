60bf62a93ce2009611ac8791c09ac67b37d642e7
omerokumus <omerokumus3@gmail.com>
2025-06-10 10:12:31 +0300

    combine started

--- iOS-Learning/ViewController.swift
+++ iOS-Learning/ViewController.swift
@@ -6,14 +6,39 @@
 //
 
 import UIKit
+import Combine
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var label: UILabel!
+    var counter = 0
+    
+    let publisher = PassthroughSubject<Int, Never>()
+    
+    let cancellables = Set<AnyCancellable>()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        bind()
     }
-
+    
+    private func bind() {
+        let s = publisher.sink { completion in
+            print("ReceiveCompletion completion: \(completion)")
+        } receiveValue: { value in
+            print("receiveValue value: \(value)")
+        }
+        
+        let myRange = (0...3)
+        let cancellable = myRange.publisher
+            .sink(receiveCompletion: { print ("completion: \($0)") },
+                  receiveValue: { print ("value: \($0)") })
+    
+        publisher.send(1)
+        publisher.send(2)
+        publisher.send(3)
+    
+    }
 
 }
 
